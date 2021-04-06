84a818ab61850317f921297edd4225f9e4531e5d
krishnamanaiducloud <49259247+krishnamanaiducloud@users.noreply.github.com>
2021-04-06 22:51:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,9 +10,9 @@ INSTANCE_TYPE="t3.xlarge"
 NS=pm
 SERIES_VALUE=10
 CIDR=172
-sed -i -e "s/REGION/$REGION/g" -e "s/STAGE/STAGE/g" -e "s/MIN/MIN/g" -e "s/MAX/MAX/g" main.tf
-sed -i -e "s/IMAGE/$IMAGE/g" -e "s/LOCATION/$LOCATION/g" -e "s/INSTANCE_TYPE/INSTANCE_TYPE/g" main.tf
-sed -i -e "s/NS/$NS/g" -e "s/SERIES_VALUE/%SERIES_VALUE%/g" -e "s/CIDR/%CIDR%/g" main.tf
+sed -i -e "s/REGION/$REGION/g" -e "s/STAGE/$STAGE/g" -e "s/MIN/$MIN/g" -e "s/MAX/$MAX/g" main.tf
+sed -i -e "s/IMAGE/$IMAGE/g" -e "s/LOCATION/$LOCATION/g" -e "s/INSTANCE_TYPE/$INSTANCE_TYPE/g" main.tf
+sed -i -e "s/NS/$NS/g" -e "s/SERIES_VALUE/$SERIES_VALUE/g" -e "s/CIDR/$CIDR%/g" main.tf
 # verify if key exists else create it
-aws ec2 create-key-pair --key-name eks%STAGE% --query 'KeyMaterial' --output text > /home/bpadmin/secret/%PROFILE%/eks%STAGE%.pem
+aws ec2 create-key-pair --key-name eks$STAGE --query 'KeyMaterial' --output text > /home/centos/keys/eks$STAGE.pem
 echo 'Created EKS Keypair'
